2f3ee0d6fe3ddd9cbfd2f3a7ccd7df0065b3ed04
Yan Mihailov <yanmhlv@gmail.com>
2016-02-28 14:26:39 +0300

    added checkResult function

---
+++ result.go
@@ -0,0 +1,30 @@
+package pcloud
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+)
+
+func checkResult(resp *http.Response, err error) error {
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	result := struct {
+		Result int    `json:"result"`
+		Error  string `json:"error"`
+	}{}
+
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return err
+	}
+
+	if result.Result != 0 {
+		return errors.New(result.Error)
+	}
+
+	return nil
+}

84cd93618579fd3e16f17975a8bc9485cda636e8
Yan Mihailov <yanmhlv@gmail.com>
2016-03-07 15:31:15 +0300

    added convertToBuffer func; checkResult use convertToBuffer function

--- result.go
+++ result.go
@@ -1,25 +1,37 @@
 package pcloud
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"net/http"
 )
 
+// convertToBuffer; convert http.Response.Body to bytes.Buffer
+func convertToBuffer(resp *http.Response, err error) (*bytes.Buffer, error) {
+	buf := &bytes.Buffer{}
+	if err != nil {
+		return buf, err
+	}
+	defer resp.Body.Close()
+
+	_, err = buf.ReadFrom(resp.Body)
+	return buf, err
+}
+
 // checkResult; returned error if request is failed or server returned error
 func checkResult(resp *http.Response, err error) error {
+	buf, err := convertToBuffer(resp, err)
 	if err != nil {
 		return err
 	}
-
-	defer resp.Body.Close()
 
 	result := struct {
 		Result int    `json:"result"`
 		Error  string `json:"error"`
 	}{}
 
-	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+	if err := json.Unmarshal(buf.Bytes(), &result); err != nil {
 		return err
 	}
 

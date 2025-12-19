4553289840c5b36a8e54c47664d397241d4ca0d1
Ian Mikhailov <1557291+yanmhlv@users.noreply.github.com>
2025-12-19 11:27:36 -0300

    refactor(file): use pipe close on upload error instead of channel drain

--- file.go
+++ file.go
@@ -149,7 +149,7 @@ func (c *Client) upload(ctx context.Context, params url.Values, filename string,
 
 	var resp uploadResponse
 	if err := c.doPost(ctx, "uploadfile", params, pr, writer.FormDataContentType(), &resp); err != nil {
-		<-errCh
+		_ = pr.CloseWithError(err)
 		return nil, err
 	}
 	if err := <-errCh; err != nil {

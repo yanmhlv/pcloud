e503780ff8e39bdb378af968a238624d949f5c14
Ian Mikhailov <1557291+yanmhlv@users.noreply.github.com>
2026-06-18 16:01:14 -0300

    refactor(search): extract applySearchOpts helper

--- search.go
+++ search.go
@@ -16,19 +16,24 @@ type searchResponse struct {
 	Items []Metadata `json:"items"`
 }
 
+func applySearchOpts(params url.Values, opts *SearchOpts) {
+	if opts == nil {
+		return
+	}
+	if opts.FolderID > 0 {
+		params.Set(paramFolderID, formatUint(opts.FolderID))
+	}
+	if opts.Recursive {
+		params.Set("recursive", "1")
+	}
+}
+
 func (c *Client) Search(ctx context.Context, query string, opts *SearchOpts) ([]Metadata, error) {
 	params := url.Values{"query": {query}}
-	if opts != nil {
-		if opts.FolderID > 0 {
-			params.Set(paramFolderID, formatUint(opts.FolderID))
-		}
-		if opts.Recursive {
-			params.Set("recursive", "1")
-		}
-	}
+	applySearchOpts(params, opts)
 
 	var resp searchResponse
-	if err := c.do(ctx, "searchfiles", params, &resp); err != nil {
+	if err := c.doGet(ctx, "searchfiles", params, &resp); err != nil {
 		return nil, fmt.Errorf("search %q: %w", query, err)
 	}
 	return resp.Items, nil
